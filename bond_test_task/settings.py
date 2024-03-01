b2593fb8ba519c51b84634f2f861003bc75d83d6
Oleksandr Mamchur <irex1988@gmail.com>
2024-03-02 00:06:55 +0200

    added link to allowed_hosts

--- bond_test_task/settings.py
+++ bond_test_task/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["127.0.0.1", "bond-test-task.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
